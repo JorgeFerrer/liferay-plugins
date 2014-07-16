c97f94a3116fec62c13ed7b9bd2bbb123f0c9235
JM <jose.navarro@liferay.com>
2014-07-16 19:28:57 +0200

    LPS-48580 Forgot password methods added. Still under development

--- portlets/mobile-widgets-compat-portlet/docroot/WEB-INF/src/com/liferay/sample/service/impl/MobileWidgetsServiceImpl.java
+++ portlets/mobile-widgets-compat-portlet/docroot/WEB-INF/src/com/liferay/sample/service/impl/MobileWidgetsServiceImpl.java
@@ -14,23 +14,21 @@
 
 package com.liferay.sample.service.impl;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
-import com.liferay.portal.NoSuchPreferencesException;
-import com.liferay.portal.UserEmailAddressException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.Company;
-import com.liferay.portal.model.PortalPreferences;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.ac.AccessControlled;
 import com.liferay.portal.service.CompanyServiceUtil;
-import com.liferay.portal.service.UserServiceUtil;
-import com.liferay.portal.service.persistence.PortalPreferencesUtil;
+import com.liferay.portal.service.PortalPreferencesLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.util.PortletKeys;
-import com.liferay.portlet.PortletPreferencesFactoryUtil;
 import com.liferay.sample.service.base.MobileWidgetsServiceBaseImpl;
 
 /**
@@ -54,94 +52,163 @@ public class MobileWidgetsServiceImpl extends MobileWidgetsServiceBaseImpl {
 	 * Never reference this interface directly. Always use {@link com.liferay.sample.service.MobileWidgetsServiceUtil} to access the mobile widgets remote service.
 	 */
 	
-	@AccessControlled(guestAccessEnabled=true)
-	public boolean resetPasswordByEmailAddress(long companyId, String emailAddress) {
-		return false;
+	static enum SentEmailForgotPasswordType {
+		SENT_PASSWORD,
+		SEND_RESET_LINK, 
+		ERROR_WRONG_CONFIGURATION;		
 	}
 	
 	@AccessControlled(guestAccessEnabled=true)
-	public boolean resetPasswordByScreenName(long companyId, String screenName) {
-		try {
-			try {
-				User user = UserServiceUtil.getUserByScreenName(companyId, screenName);
-				return true;
-				//_sendPassword(user);
+	public Map<String, Object> resetPasswordByEmailAddress(
+		long companyId, String emailAddress) {
 
-			} catch (UserEmailAddressException e) {
-				e.printStackTrace();
-				return false;
-			} catch (PortalException e) {
-				e.printStackTrace();
-				return false;
-			}
-		} catch (SystemException e) {
-			e.printStackTrace();
-			return false;
+		try {
+			User user = UserLocalServiceUtil.getUserByEmailAddress(
+					companyId, emailAddress);
+			
+			SentEmailForgotPasswordType sendPasswordResult = 
+					_sendPassword(user);
+			
+			return _getMapFromSendPasswordResult(sendPasswordResult);
+
+		} catch (Exception exception) {
+			return _resultMapForException(exception);
 		}
 	}
 	
 	@AccessControlled(guestAccessEnabled=true)
-	public boolean resetPasswordByUserId(long userId) {
-		return false;
-	}
-	
-	/*
-	private Map<String, String> _sendPassword(User user) {
+	public Map<String, Object> resetPasswordByScreenName(
+		long companyId, String screenName) {
+
 		try {
-			try {
-				Company company = CompanyServiceUtil.getCompanyById(user.getCompanyId());
-				
-				if (!company.isSendPassword() && !company.isSendPasswordResetLink()) {
-					return null;
-				}
-				
-				PortalPreferences portalPreferences = _getPortalPreferences(user.getCompanyId());
-				
-				portalPreferences.get
-				
-				String emailFromName = portalPreferences.getValue(
-						"emailFromName", null);
-					String emailFromAddress = portalPreferences.getValue(
-						"emailFromAddress", null);
-					String emailToAddress = user.getEmailAddress();
+			User user = UserLocalServiceUtil.getUserByScreenName(
+					companyId, screenName);
+			
+			SentEmailForgotPasswordType sendPasswordResult = 
+					_sendPassword(user);
+			
+			return _getMapFromSendPasswordResult(sendPasswordResult);
 
-					String emailParam = "emailPasswordSent";
-
-					if (company.isSendPasswordResetLink()) {
-						emailParam = "emailPasswordReset";
-					}
-
-				
-				portalPreferences.get
-				
-
-			} catch (UserEmailAddressException e) {
-				e.printStackTrace();
-				return null;
-			} catch (PortalException e) {
-				e.printStackTrace();
-				return null;
-			}
-		} catch (SystemException e) {
-			e.printStackTrace();
-			return null;
+		} catch (Exception exception) {
+			return _resultMapForException(exception);
 		}
-
-		
-
-
-		
 	}
 	
-	private PortalPreferences _getPortalPreferences(long companyId) 
-			throws NoSuchPreferencesException, SystemException {
+	@AccessControlled(guestAccessEnabled=true)
+	public Map<String, Object> resetPasswordByUserId(long userId) {
+		try {
+			User user = UserLocalServiceUtil.getUser(userId);
+			
+			SentEmailForgotPasswordType sendPasswordResult = 
+					_sendPassword(user);
+			
+			return _getMapFromSendPasswordResult(sendPasswordResult);
+
+		} catch (Exception exception) {
+			return _resultMapForException(exception);
+		}
+	}
+	
+	private Map<String, Object> _getMapFromSendPasswordResult(
+		SentEmailForgotPasswordType sendPasswordResult) {
+
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+
+		if (sendPasswordResult == 
+				SentEmailForgotPasswordType.SENT_PASSWORD || 
+			sendPasswordResult == 
+				SentEmailForgotPasswordType.SEND_RESET_LINK) {
+			
+			resultMap.put("success", sendPasswordResult.toString());
+		}
+		else {
+			resultMap.put("error", sendPasswordResult.toString());
+		}
+		
+		return resultMap;
+	}
+
+	private Map<String, Object> _resultMapForException(Exception exception) {
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		
+		_fillResultMapForException(resultMap, exception);
+		
+		return resultMap;
+	}
+	
+	private void _fillResultMapForException(
+			Map<String, Object> resultMap, Exception exception) {
+
+		resultMap.put("exception.className", exception.getClass().getName());
+		resultMap.put("exception.message", exception.getMessage());
+		resultMap.put(
+				"exception.localizedMessage", exception.getLocalizedMessage());
+	}
+
+	private SentEmailForgotPasswordType _sendPassword(User user) 
+			throws PortalException, SystemException {
+		
+		Company company = CompanyServiceUtil.getCompanyById(
+				user.getCompanyId());
+		
+		if (!company.isSendPassword() && !company.isSendPasswordResetLink()) {
+			return SentEmailForgotPasswordType.ERROR_WRONG_CONFIGURATION;
+		}
+		
+		PortletPreferences portalPreferences = 
+				_getPortalPreferences(user.getCompanyId());
+		
+		String emailFromName = portalPreferences.getValue(
+			"emailFromName", null);
+		String emailFromAddress = portalPreferences.getValue(
+			"emailFromAddress", null);
+
+		String emailParam = "emailPasswordSent";
+		SentEmailForgotPasswordType emailType = 
+				SentEmailForgotPasswordType.SENT_PASSWORD;
+
+		if (company.isSendPasswordResetLink()) {
+			emailParam = "emailPasswordReset";
+			emailType = SentEmailForgotPasswordType.SEND_RESET_LINK;
+		}
+		
+		String languageId = user.getLanguageId();
+		
+		String subject = portalPreferences.getValue(
+			emailParam + "Subject_" + languageId, null);
+		String body = portalPreferences.getValue(
+			emailParam + "Body_" + languageId, null);
+		
+		ServiceContext serviceContext = _getServiceContext();
+		
+		UserLocalServiceUtil.sendPassword(
+			user.getCompanyId(), user.getEmailAddress(), emailFromName,  
+			emailFromAddress, subject, body, serviceContext);
+
+		return emailType;
+	}
+	
+	private ServiceContext _getServiceContext() {
+		ServiceContext serviceContext = new ServiceContext();
+		
+		// TODO what values set here?
+		serviceContext.setPortalURL("portal_url");
+		serviceContext.setPathMain("path_main");
+		serviceContext.setPlid(999);
+		serviceContext.setRemoteAddr("remote_addr");
+		serviceContext.setRemoteHost("remote_host");
+		
+		return serviceContext;
+	}
+
+	private PortletPreferences _getPortalPreferences(long companyId) 
+			throws SystemException {
 		
 		long ownerId = companyId;
 		int ownerType = PortletKeys.PREFS_OWNER_TYPE_COMPANY;
 		
-		return PortalPreferencesUtil.findByO_O(ownerId, ownerType);
+		return PortalPreferencesLocalServiceUtil.getPreferences(
+			ownerId, ownerType);
 	}
-	
-	*/
 	
 }
