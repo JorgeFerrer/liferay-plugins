9230283e98414eb6f7eb10fef270684f74764dbb
JM <jose.navarro@liferay.com>
2014-07-21 13:20:37 +0200

    LPS-48580 Added remember password logic

--- portlets/mobile-widgets-portlet/docroot/WEB-INF/src/com/liferay/mobile/widgets/service/impl/MobileWidgetsUserServiceImpl.java
+++ portlets/mobile-widgets-portlet/docroot/WEB-INF/src/com/liferay/mobile/widgets/service/impl/MobileWidgetsUserServiceImpl.java
@@ -15,9 +15,20 @@
 package com.liferay.mobile.widgets.service.impl;
 
 import com.liferay.mobile.widgets.service.base.MobileWidgetsUserServiceBaseImpl;
+import com.liferay.mobile.widgets.service.exception.RememberPasswordException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.User;
 import com.liferay.portal.security.ac.AccessControlled;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.util.PortletKeys;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.portlet.PortletPreferences;
 
 /**
  * The implementation of the mobile widgets user remote service.
@@ -48,12 +59,18 @@ public class MobileWidgetsUserServiceImpl
 	 * {@link com.liferay.mobile.widgets.service.MobileWidgetsUserServiceUtil}
 	 * to access the mobile widgets user remote service.
 	 */
+
 	@AccessControlled(guestAccessEnabled = true)
 	public Map<String, Object> resetPasswordByEmailAddress(
 			long companyId, String emailAddress)
 		throws Exception {
 
-		return null;
+		User user = userLocalService.getUserByEmailAddress(
+			companyId, emailAddress);
+
+		RememberPasswordResult sendPasswordResult = sendRememberEmail(user);
+
+		return getResultMap(user, sendPasswordResult);
 	}
 
 	@AccessControlled(guestAccessEnabled = true)
@@ -61,14 +78,103 @@ public class MobileWidgetsUserServiceImpl
 			long companyId, String screenName)
 		throws Exception {
 
-		return null;
+		User user = userLocalService.getUserByScreenName(companyId, screenName);
+
+		RememberPasswordResult sendPasswordResult = sendRememberEmail(user);
+
+		return getResultMap(user, sendPasswordResult);
 	}
 
 	@AccessControlled(guestAccessEnabled = true)
 	public Map<String, Object> resetPasswordByUserId(long userId)
 		throws Exception {
 
-		return null;
+		User user = userLocalService.getUserById(userId);
+
+		RememberPasswordResult sendPasswordResult = sendRememberEmail(user);
+
+		return getResultMap(user, sendPasswordResult);
 	}
+
+	protected PortletPreferences getPortalPreferences(long companyId)
+		throws SystemException {
+
+		long ownerId = companyId;
+		int ownerType = PortletKeys.PREFS_OWNER_TYPE_COMPANY;
+
+		return portalPreferencesLocalService.getPreferences(ownerId, ownerType);
+	}
+
+	protected Map<String, Object> getResultMap(
+		User user, RememberPasswordResult sendPasswordResult) {
+
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+
+		resultMap.put("email", user.getEmailAddress());
+		resultMap.put("success", StringUtil.toLowerCase(
+			sendPasswordResult.toString()));
+
+		return resultMap;
+	}
+
+	protected ServiceContext getServiceContext() {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		// TODO what values set here?
+
+		serviceContext.setPortalURL("portal_url");
+		serviceContext.setPathMain("path_main");
+		serviceContext.setPlid(999);
+		serviceContext.setRemoteAddr("remote_addr");
+		serviceContext.setRemoteHost("remote_host");
+
+		return serviceContext;
+	}
+
+	protected RememberPasswordResult sendRememberEmail(User user)
+		throws PortalException, SystemException {
+
+		Company company = companyLocalService.getCompanyById(
+			user.getCompanyId());
+
+		if (!company.isSendPassword() && !company.isSendPasswordResetLink()) {
+			throw new RememberPasswordException();
+		}
+
+		PortletPreferences portalPreferences = getPortalPreferences(
+			user.getCompanyId());
+
+		String emailFromName = portalPreferences.getValue(
+			"emailFromName", null);
+		String emailFromAddress = portalPreferences.getValue(
+			"emailFromAddress", null);
+
+		String emailParam = "emailPasswordSent";
+		RememberPasswordResult emailType = RememberPasswordResult.SENT_PASSWORD;
+
+		if (company.isSendPasswordResetLink()) {
+			emailParam = "emailPasswordReset";
+			emailType = RememberPasswordResult.SENT_RESET_LINK;
+		}
+
+		String languageId = user.getLanguageId();
+
+		String subject =
+			portalPreferences.getValue(
+				emailParam + "Subject_" + languageId, null);
+		String body = portalPreferences.getValue(
+			emailParam + "Body_" + languageId, null);
+
+		ServiceContext serviceContext = getServiceContext();
+
+		userLocalService.sendPassword(
+			user.getCompanyId(), user.getEmailAddress(), emailFromName,
+			emailFromAddress, subject, body, serviceContext);
+
+		return emailType;
+	}
+
+	protected enum RememberPasswordResult {SENT_PASSWORD, SENT_RESET_LINK}
 
 }
